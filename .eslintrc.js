12a538960f2fc53fb97dd762e891497482a77de6
Ben Ogle <ogle.ben@gmail.com>
2020-12-12 12:52:26 -0800

    Add ability to ref by abosulte path

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,3 +1,17 @@
 module.exports = {
   extends: ['nicenice'],
+  settings: {
+    'import/resolver': {
+      'babel-plugin-root-import': {
+        rootPathSuffix: 'src',
+      },
+      node: {
+        moduleDirectory: [
+          'node_modules',
+          'src',
+          '.',
+        ],
+      },
+    },
+  },
 }
