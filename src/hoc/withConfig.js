11048d2d30725354d1c727cb2778bb08868e7b16
Ben Ogle <ogle.ben@gmail.com>
2021-01-23 11:46:09 -0800

    Fix filewatcher when editing in json editor

--- src/hoc/withConfig.js
+++ src/hoc/withConfig.js
@@ -1,7 +1,9 @@
+import debounce from 'lodash/debounce'
 import React from 'react'
 import UserConfigStore from 'common/UserConfigStore'
+import req from 'common/req'
 
-const userConfigStore = new UserConfigStore({ watch: true })
+const fs = req('fs')
 
 // Will inject the user config in as a `config` param. Watches for changes.
 //
@@ -12,24 +14,52 @@ const userConfigStore = new UserConfigStore({ watch: true })
 // withConfig()(SomeView)
 const withConfig = (options) => (Component) => {
   class WithConfig extends React.Component {
-    state = {
-      userConfig: userConfigStore.getUserConfig(),
+    constructor (props) {
+      super(props)
+      this.userConfigStore = new UserConfigStore()
+      this.state = {
+        userConfig: this.userConfigStore.getUserConfig(),
+      }
     }
 
     componentDidMount () {
-      this.configDisposable = userConfigStore.onDidAnyChange(this.updateConfig)
+      this.watchConfigFile()
     }
 
     componentWillUnmount (nextProps) {
-      this.configDisposable()
+      this.cleanUpWatcher()
     }
 
-    updateConfig = (newStore, prevStore) => {
-      this.setState({
-        userConfig: userConfigStore.getUserConfig(),
-        prevUserConfig: userConfigStore.getUserConfig(prevStore),
-      })
+    cleanUpWatcher = () => {
+      if (this.watcher) {
+        this.watcher.close()
+        this.watcher = null
+      }
     }
+
+    watchConfigFile = () => {
+      this.cleanUpWatcher()
+
+      // Save current config so child components can compare what changed on next change
+      this.prevUserConfigValue = this.userConfigStore.store
+
+      // https://nodejs.org/docs/latest/api/fs.html#fs_class_fs_fswatcher
+      this.watcher = fs.watch(this.userConfigStore.path, { persistent: false }, this.updateConfig)
+    }
+
+    updateConfig = debounce(() => { // eslint-disable-line react/sort-comp
+      this.setState({
+        userConfig: this.userConfigStore.getUserConfig(),
+        prevUserConfig: this.userConfigStore.getUserConfig(this.prevUserConfigValue),
+      })
+
+      // HACK: ideally calling watch only on mount would be all we need to do.
+      // But I can't get the file watcher to work reliably more than once for
+      // edits within the app. e.g. open the UserConfigPage window and edit a
+      // thing 2x, the handler would only be called once. This will setup the
+      // file watcher again after each change.
+      this.watchConfigFile()
+    }, 100)
 
     render () {
       return (
